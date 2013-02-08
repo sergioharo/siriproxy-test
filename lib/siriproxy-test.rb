587a07c386d25095841d5e9fc23b93a057c6d005
sergioharo <sergio.haro.jr@gmail.com>
2013-02-08 14:47:00 -0800

    adding more options and fixing regex to allow more phrases

--- lib/siriproxy-test.rb
+++ lib/siriproxy-test.rb
@@ -12,10 +12,25 @@ require 'pp'
 
 class SiriProxy::Plugin::Test < SiriProxy::Plugin
   def initialize(config)
-    @sergioThoughts = ["He's awesome!"]
-    @julieThoughts = ["She's nice!"]
-    @christineThoughts = ["I don't like her."]
-    @otherThoughts = ["They're ok."]
+    @sergioThoughts = [
+      "He's awesome!", 
+      "He's the best!", 
+      "Just let go and let the magic happen."
+    ]
+    @julieThoughts = [
+      "She's nice!", 
+      "Her face is round.", 
+      "She really likes purple...and chocolate."
+    ]
+    @christineThoughts = [
+      "I don't like her.", 
+      "Is she around, I don't want her to mace me.", 
+      "She's a tele-tubby."]
+    @otherThoughts = [
+      "They're ok.", 
+      "Eh, I don't know them that well.", 
+      "Are they the one who...nevermind I won't get into that."
+    ]
     @christineJokes = [
       "Christine's face.", 
       "Why did the chicken cross the road? It saw Christine's face.",
@@ -29,7 +44,7 @@ class SiriProxy::Plugin::Test < SiriProxy::Plugin
     say thought
   end
 
-  listen_for /what do you think of (.*)/i do |name|
+  listen_for /(what|how).+you (think|like)( ?of)? (.*)/i do |a,b,c,name|
     if name =~ /sergio/i then
       sayFromArray @sergioThoughts
     elsif name =~ /julie/i then
@@ -43,7 +58,7 @@ class SiriProxy::Plugin::Test < SiriProxy::Plugin
     request_completed 
   end
 
-  listen_for /joke/i do
+  listen_for /(tell|have|say) .*joke/i do
     sayFromArray @christineJokes
     request_completed
   end
